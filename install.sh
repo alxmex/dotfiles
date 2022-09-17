c471c29592afef68c725d2ea570408c16901f42c
Alexander Mehks <54999803+alexandermehks@users.noreply.github.com>
2022-09-17 12:19:06 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+########
+
+# Author: Alexander Mehks
+# Date: 22-09-17
+# Description: Installation of my config files.
+
+########
+
+HOME_CONFIG_PATH="/home/$USER/"
+
+#If something goes wrong
+mkdir config_backups
+cp $HOME_CONFIG_PATH.tmux.conf $HOME_CONFIG_PATH.bashrc config_backups
+cp -r $HOME_CONFIG_PATH.config/alacritty config_backups
+cp -r $HOME_CONFIG_PATH.config/nvim config_backups
+cp -r $HOME_CONFIG_PATH.local/share/fonts config_backups
+
+#Single files
+cp .tmux.conf $HOME_CONFIG_PATH
+cp .bashrc $HOME_CONFIG_PATH
+
+#Alacritty
+cp -r alacritty $HOME_CONFIG_PATH.config
+
+#Nvim
+cp -r nvim $HOME_CONFIG_PATH.config
+
+#Fonts
+cp -r fonts $HOME_CONFIG_PATH.local/share
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
