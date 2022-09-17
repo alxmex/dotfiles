c254902ee69d31738707affef92cd9114ad2cb1b
Alexander Mehks <54999803+alexandermehks@users.noreply.github.com>
2022-09-17 11:30:26 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,11 +3,10 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.api.nvim_command("set noswapfile")
 
-
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 vim.o.relativenumber = true
 
+require('keybindings')
 require('packer-plugins')
 require("nvim-tree").setup()
-
-
+require("telescope").setup()
