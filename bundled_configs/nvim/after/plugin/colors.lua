f9eb6fbafc3924d57523121425da170b0bd0d7e4
pingen <hehe.@gmail.com>
2023-04-14 18:31:08 +0200

    update

--- bundled_configs/nvim/after/plugin/colors.lua
+++ bundled_configs/nvim/after/plugin/colors.lua
@@ -2,9 +2,8 @@ function setColor(color)
     color = color or "rose-pine"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
 end
 
 setColor("base16-twilight")
--- bundled_configs/nvim/lua/alxmex/setup.lua
+++ bundled_configs/nvim/lua/alxmex/setup.lua
@@ -2,8 +2,6 @@ vim.opt.guicursor=""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4 
@@ -24,6 +22,4 @@ vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
