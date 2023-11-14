529e5e6e1047927ee9d43bd3404896b174f6db98
alexandermehks <erks1@protonmail.com>
2023-11-14 21:12:21 +0100

    update

--- scripts/startup_scripts/background.sh
+++ scripts/startup_scripts/background.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-feh --bg-fill /home/$USER/dotfiles/startup_scripts/bild2.jpg
+#feh --bg-fill /home/$USER/dotfiles/startup_scripts/bild2.jpg
 #feh --bg-fill /home/$USER/dotfiles/startup_scripts/curr_background2.png
